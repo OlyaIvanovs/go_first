a37e2dcd8c6a04dcd196ab08c23c09bf6c6d2f4b
olya <olya@ivanovs.info>
2021-03-26 12:09:17 +1300

    Sorting and anagram check

--- chapter3/comma.go
+++ chapter3/comma.go
@@ -4,6 +4,8 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"sort"
+	"strings"
 )
 
 func comma(s string) string {
@@ -14,9 +16,18 @@ func comma(s string) string {
 	return comma(s[:n-3]) + "," + s[n-3:]
 }
 
-// None-recursive version of the func above
+// None-recursive version of the func above. It deals also with floating-point numbers
 func comma2(s string) string {
 	var buf bytes.Buffer
+
+	index := strings.Index(s, ".")
+	var endString string
+	if index != -1 {
+		endString = s[index:]
+		s = s[:index]
+		fmt.Println(endString)
+	}
+
 	n := len(s)
 	commaStep := 3
 	comma := 3
@@ -33,7 +44,25 @@ func comma2(s string) string {
 			comma += commaStep
 		}
 	}
+	if endString != "" {
+		fmt.Fprintf(&buf, "%s", endString)
+	}
 	return buf.String()
+}
+
+func sortString(s string) string {
+	snew := strings.Split(s, "")
+	sort.Strings(snew)
+	return strings.Join(snew, "")
+}
+
+func anagramCheck(s1, s2 string) bool {
+	sortedS1 := sortString(s1)
+	sortedS2 := sortString(s2)
+	if sortedS1 == sortedS2 {
+		return true
+	}
+	return false
 }
 
 func main() {
@@ -41,6 +70,9 @@ func main() {
 	// n := len(s)
 	// fmt.Print(s[:n-3])
 	// fmt.Print(s[:3])fmt.Print(s[:3])
-	s := comma2("134567890")
+	s := comma2("134567.890")
+	s2 := comma2("134567890")
 	fmt.Println(s)
+	fmt.Println(s2)
+	fmt.Println(anagramCheck("2a3", "a23"))
 }
